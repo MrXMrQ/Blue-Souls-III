ade53df9f2c8c8a72a1e04025caa71701b057f04
MrXMrQ <90854368+MrXMrQ@users.noreply.github.com>
2023-04-10 17:30:38 +0200

    .

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        MyScreen myScreen = new MyScreen();
+        myScreen.add(new Game());
+    }
+}
---
+++ src/MyScreen.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyScreen extends JFrame {
+    public MyScreen() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(new Dimension(500, 500));
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(true);
+    }
+}
